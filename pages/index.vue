520185529201c0353c8b336d30f914d594313d8c
cgobbet <cassiano.gobbet@gmail.com>
2020-05-26 15:12:42 +0200

    Replace template content

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,31 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        zeitgeist-blog
-      </h1>
-      <h2 class="subtitle">
-        Headless WordPress Site On The JAMstack
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+<div class="posts">
+  <main>
+    <h2>Posts</h2>
+  <!-- here we loop through the posts -->
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h3>
+      <!-- for each one of them, we’ll render their title, and link off to their individual page -->
+        <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
+      </h3>
+      <div v-html="post.excerpt.rendered"></div>
+      <a :href="`blog/${post.slug}`" class="readmore">Read more ⟶</a>
     </div>
+  </main>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+computed: {
+  posts() {
+    return this.$store.state.posts;
+  },
+ },
+created() {
+  this.$store.dispatch("getPosts");
+},
+};
 </script>
 
 <style>
