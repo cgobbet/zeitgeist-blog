51adc499a93ea35fa3775543ba77cd18e3a6f7ea
cgobbet <cassiano.gobbet@gmail.com>
2020-05-26 15:31:19 +0200

    Update index.js
    Fixes to small bugs, replacement of dummy URL for definitive one

--- store/index.js
+++ store/index.js
@@ -1,21 +1,6 @@
-// state holds the posts, or whatever info we need to store
-// mutations will hold functions that will update the state.
-// actions can make asynchronous API calls
-
-/*
-this is where we will eventually hold the data for all 
-of our posts
-*/
 export const state = () => ({
-	'posts: []',
-	// value: 'posts: []',
+	posts: [],
 });
-
-export const getters = {
-	getterValue: state => {
-		return state.posts;
-	},
-};
 
 export const mutations = {
 	updatePosts: (state, posts) => {
@@ -27,12 +12,12 @@ export const actions = {
  async getPosts({ state, commit }) {
    if (state.posts.length) return
    try {
-     let posts = await fetch( `https://thinking.media/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+     let posts = await fetch( `https://zeitgeist.digital/wp-json/wp/v2/posts`
      ).then(res => res.json())
      posts = posts
        .filter(el => el.status === "publish")
        .map(({ id, slug, title, excerpt, date, tags, content }) => ({
-         id
+         id,
          slug,
          title,
          excerpt,
@@ -40,8 +25,9 @@ export const actions = {
          tags,
          content
        }))
-     commit("updatePosts", posts)
+     commit('updatePosts', posts)
    } catch (err) {
      console.log(err)
    }
+}
 }
