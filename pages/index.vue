c3ef7a1ba9c237806d76d9a3cb8b0c7ce6d5fa92
cgobbet <cassiano.gobbet@gmail.com>
2020-05-26 15:54:50 +0200

    Update index.vue
    Tweaks in CSS and wording

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="posts">
   <main>
-    <h2>Posts</h2>
+    <h2>Digital Zeitgeist</h2>
+    <h4>Life read in Ones and Zeroes</h4>
   <!-- here we loop through the posts -->
     <div class="post" v-for="post in posts" :key="post.id">
       <h3>
@@ -9,7 +10,7 @@
         <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
       </h3>
       <div v-html="post.excerpt.rendered"></div>
-      <a :href="`blog/${post.slug}`" class="readmore">Read more ⟶</a>
+      <a :href="`blog/${post.slug}`" class="readmore">Continue...</a>
     </div>
   </main>
   </div>
@@ -29,6 +30,42 @@ created() {
 </script>
 
 <style>
+body {
+  padding: 0.3rem;
+}
+
+a {
+  text-decoration: none;
+  /* text-decoration-color: #21ff21; */
+}
+
+a.readmore {
+  font-family: 'ITC Bauhaus', sans-serif;
+  font-size: 0.8em;
+}
+
+h2 {
+  font-family: 'Futura', sans-serif;
+  font-size: 2rem;
+  padding: 1rem 0 1rem 0;  
+}
+
+h3 {
+  font-family: 'Oswald', sans-serif;
+  color: 'black;';
+  font-weight: regular;
+  padding: 1rem 0 1rem 0;
+}
+
+h4 {
+  font-family: 'ITC Bauhaus', sans-serif;
+  font-size: 1rem;  
+}
+
+p {
+  font-family: 'Ideal-Grotesk', sans-serif;
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -48,10 +85,10 @@ created() {
   letter-spacing: 1px;
 }
 
-.subtitle {
+.h4 {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: "black";
   word-spacing: 5px;
   padding-bottom: 15px;
 }
