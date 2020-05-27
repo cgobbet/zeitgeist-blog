f23b15b488e5e34221ea45e32abddd358ec4a581
cgobbet <cassiano.gobbet@gmail.com>
2020-05-27 10:50:34 +0200

    Update nuxt.config.js
    Switch WP URL data source

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let dynamicRoutes = () => {
   const routes = axios
-    .get("https://thinking.media/wp-json/wp/v2/posts?page=1&per_page=20")
+    .get("https://zeitgeist.digital/wp-json/wp/v2/posts?page=1&per_page=20")
     .then(res => {
       return res.data.map(post => `/blog/${post.slug}`)
     })
