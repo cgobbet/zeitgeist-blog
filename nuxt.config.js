50f0c9e0dfdd432e1c38e7c07ef9ddde28ba3162
cgobbet <cassiano.gobbet@gmail.com>
2020-05-27 09:41:12 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,50 @@
-import axios from 'axios';
+import axios from "axios"
 let dynamicRoutes = () => {
-	return axios.get('https://zeitgeist.digital/wp-json/wp/v2/posts').then(res => {
-		return res.data.map(post => `/blog/${post.slug}`);
-	});
-};
+  const routes = axios
+    .get("https://thinking.media/wp-json/wp/v2/posts?page=1&per_page=20")
+    .then(res => {
+      return res.data.map(post => `/blog/${post.slug}`)
+    })
+  console.log(routes)
+  return routes
+}
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Alata|Open+Sans&display=swap"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: ["~/assets/mixins.scss"],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     "~/plugins/posts.server.js",
     "~/plugins/tags.server.js",
@@ -42,23 +54,16 @@ export default {
     routes: dynamicRoutes
   },
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
